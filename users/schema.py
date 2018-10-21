dbc92f3da83f90aa35fcb86de21e9cd427891407
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2018-10-22 08:16:41 +0900

    UserType

--- users/schema.py
+++ users/schema.py
@@ -5,8 +5,14 @@ from . import models
 
 class UserType(DjangoObjectType):
 
+    full_name = graphene.String()
+
     class Meta:
         model = models.User
+        exclude_fields = ('password',)
+
+    def resolve_full_name(self, info):
+        return f'{self.first_name} {self.last_name}'
 
 
 class Query(object):
